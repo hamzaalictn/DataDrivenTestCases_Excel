c1bd66bbcf3505239498072c234672c851dfe516
hamzaalictn <r.hmzctn@gmail.com>
2021-03-07 08:16:08 +0100

    test3

--- src/test/java/dailyCodePractice/Test.java
+++ src/test/java/dailyCodePractice/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("dummy");
         System.out.println("dummy1");
+        System.out.println("dummy2");
     }
 }
